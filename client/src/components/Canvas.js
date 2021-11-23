a72577ea3c1625caf37b3d135bc02401d421aca7
Jose Flores <flores.jdfr@gmail.com>
2021-11-22 20:45:01 -0600

    Se pueden crear edges con la tecla "e" y agregar nodes con doble clic

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -45,39 +45,96 @@ export const Canvas = () => {
 
     const createNode = (x, y) => {
         const color = randomColor();
-        setCanvas(({ graph: { nodes, edges, nodesCounter, ...restInGraph }, ...restInFA }) => {
-            const id = nodesCounter + 1;
-            const from = Math.floor(Math.random() * (nodesCounter - 1)) + 1;
-            let newNode = {
+
+        //Verify if nodes are empty, that means the canvas is empty
+        if (!graph.nodes) {
+            setCanvas(() => {
+                const id = 1;
+                let newNode = {
+                    graph: {
+                        nodes: [
+                            { id, label: `Node ${id}`, color, x, y }
+                        ],
+                        nodesCounter: id,
+                        edges: [],
+                        edgesCounter: 0
+                    }
+                }
+                console.log("New node: ", newNode);
+                return newNode;
+            });
+        }
+        else {
+            setCanvas(({ graph: { nodes, edges, nodesCounter, ...restInGraph }, ...restInFA }) => {
+                const id = nodesCounter + 1;
+                const from = Math.floor(Math.random() * (nodesCounter - 1)) + 1;
+                let newNode = {
+                    graph: {
+                        nodes: [
+                            ...nodes,
+                            { id, label: `Node ${id}`, color, x, y }
+                        ],
+                        edges: [
+                            ...edges,
+                        ],
+                        nodesCounter: id,
+                        ...restInGraph
+                    },
+                    ...restInFA
+                }
+                console.log("New node: ", newNode);
+                return newNode;
+            });
+        }
+
+    }
+
+    const createEdge = () => {
+
+        const convertNodeToEdge = (nodesArray, edgesCounter) => {
+            let edges = []
+            nodesArray.forEach((node, i) => {
+
+                let newID = ++edgesCounter;
+                edges.push({
+                    id: newID,
+                    from: nodesArray[0],
+                    to: node,
+                    label: "New Transition"
+                })
+
+            });
+            return { newEdges: edges, newID: edgesCounter };
+        }
+
+        setCanvas(({ graph: { edges, edgesCounter, ...restInGraph }, selectedNodes, ...restInFA }) => {
+            let { newEdges, newID } = convertNodeToEdge(selectedNodes, edgesCounter);
+            let newGraph = {
                 graph: {
-                    nodes: [
-                        ...nodes,
-                        { id, label: `Node ${id}`, color, x, y }
-                    ],
-                    edges: [
-                        ...edges,
-                    ],
-                    nodesCounter: id,
+                    edges: edges.concat(newEdges),
+                    edgesCounter: newID,
                     ...restInGraph
                 },
                 ...restInFA
             }
-            console.log("New node: ", newNode);
-            return newNode;
+            console.log("New graph: ", newGraph);
+            return newGraph;
         });
     }
 
     const selectNodes = (nodesArray) => {
-        setCanvas(({selectedNodes, ...rest}) => {
-          return {selectedNodes:nodesArray, ...rest}  
+        setCanvas(({ selectedNodes, ...rest }) => {
+            return { selectedNodes: nodesArray, ...rest }
         });
     }
 
     const deselectNodes = () => {
-        setCanvas(({selectedNodes, ...rest}) => {
-            return {selectedNodes:[], ...rest}  
-          });
+        setCanvas(({ selectedNodes, ...rest }) => {
+            return { selectedNodes: [], ...rest }
+        });
     }
+
+
 
     const events = {
         selectNode: ({ nodes }) => {
@@ -87,16 +144,22 @@ export const Canvas = () => {
             /* alert("Selected node: " + nodes); */
         },
         deselectNode: (nodes) => {
-            console.log("Deselected nodes:");
             deselectNodes()
-        },
-        doubleClick: ({ pointer: { canvas } }) => {
-            createNode(canvas.x, canvas.y);
         }
     }
 
     return (
-        <div className="canvas" id="canvas">
+        <div className="canvas" id="canvas"
+
+            onDoubleClick={(e) => {
+                console.log(e);
+                createNode(e.clientX, e.clientY);
+            }}
+            onKeyPress={(e) => {
+                console.log(e);
+                createEdge();
+            }}
+        >
             {
                 Object.keys(graph).length > 0 //Checks if graphs has elements
                     ? <Graph graph={graph} options={options} events={events} />
