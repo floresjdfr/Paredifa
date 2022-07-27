d79bb7db86d8e0c18fa8739b01edb03e8990607e
Jose David Flores <flores.jdfr@gmail.com>
2022-07-26 20:13:27 -0600

    minimal changes

--- client/src/index.js
+++ client/src/index.js
@@ -13,16 +13,12 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}>
       <App />
     </Auth0Provider>
   </React.StrictMode>,
